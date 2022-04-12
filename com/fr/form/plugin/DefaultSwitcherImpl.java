dcdb89bd316f008fe462996c1528aefc2c3d68c7
HuangDongQiu <43603713+HuangDongQiu@users.noreply.github.com>
2022-04-12 15:09:18 +0800

    Create DefaultSwitcherImpl.java

---
+++ com/fr/form/plugin/DefaultSwitcherImpl.java
@@ -0,0 +1,15 @@
+package com.fr.form.plugin;
+
+import com.fr.stable.fun.impl.AbstractWidgetSwitcher;
+
+public class DefaultSwitcherImpl extends AbstractWidgetSwitcher {
+  public String toNewMarkType(String paramString) {
+    return paramString;
+  }
+}
+
+
+/* Location:              C:\Users\Administrator\Downloads\fr-report-8.0.jar!\com\fr\form\plugin\DefaultSwitcherImpl.class
+ * Java compiler version: 5 (49.0)
+ * JD-Core Version:       1.1.3
+ */
